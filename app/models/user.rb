9b0173953ae091aa11b85063327a82cf4c9c838f
Julien Duponchelle <julien@duponchelle.info>
2022-01-09 21:57:40 +0100

    Send devise email via ActiveJob

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          :timeoutable
+
+  # Use ActiveJob to send emails
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
 end
